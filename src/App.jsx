a2a762cae1f594407fe490ff92490065f7159e39
Adil Allahdad <124179711+AdilAllahdad@users.noreply.github.com>
2025-03-05 09:12:40 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import React from "react";
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import About from './components/about';
+import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
